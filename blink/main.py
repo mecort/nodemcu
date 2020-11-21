df5758974aad9e000ef5a5eafbf6fb92b33df4f4
Michael Cort <mcort@uw.edu>
2020-11-20 17:58:20 -0800

    Completed lesson activity. had a blast!

--- blink/main.py
+++ blink/main.py
@@ -2,11 +2,26 @@ import time
 
 from machine import Pin
 
+response_template = """HTTP/1.0 200 OK
 
-led = Pin(2, Pin.OUT)
-for i in range(3):
-    time.sleep(.5)
-    led.value(1)
-    time.sleep(.5)
-    led.value(0)
+%s
+"""
 
+pin = Pin(9, Pin.OUT)
+
+def light_on():
+     pin.value(1)
+     body = "You turned a light on!"
+     return response_template % body
+
+def light_off():
+     pin.value(0)
+     body = "You turned a light off!"
+     return response_template % body
+
+handlers = {
+    'time': time,
+    'dummy': dummy,
+    'light_on': light_on,
+    'light_off': light_off,
+}
--- flexible_web_server/main.py
+++ flexible_web_server/main.py
@@ -46,9 +46,36 @@ def dummy():
 
     return response_template % body
 
+
+pin = machine.Pin(5, machine.Pin.OUT)
+switch_pin = machine.Pin(10, machine.Pin.IN)
+adc = machine.ADC(0)
+
+def light_on():
+    pin.value(1)
+    body = "You turned a light on!"
+    return response_template % body
+
+def light_off():
+    pin.value(0)
+    body = "You turned a light off!"
+    return response_template % body
+
+def switch():
+    body = "{state: " + str(switch_pin.value()) + "}"
+    return response_template % body
+
+def light():
+    body = "{Photoresistor value: " + str(adc.read()) + "}"
+    return response_template % body
+
 handlers = {
     'time': time,
     'dummy': dummy,
+    'light_on': light_on,
+    'light_off': light_off,
+    'switch': switch,
+    'light': light,
 }
 
 def main():
